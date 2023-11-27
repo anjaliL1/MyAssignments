492df82498039da202b8a990c8c8e2a4057110a0
anjaliL1 <151993042+anjaliL1@users.noreply.github.com>
2023-11-27 21:47:09 +0530

    Update BikeClass.java

--- week2/day1/BikeClass.java
+++ week2/day1/BikeClass.java
@@ -8,13 +8,9 @@ public class BikeClass extends Car{
 	 c.applyBreak();
 	 c.soundHorn();
 	 BikeClass b = new BikeClass();
-	 System.out.println("This is for class Car");
+	 System.out.println("This is for class Bike");
 	 b.applyBreak();
 	 b.soundHorn();
-	 /*
-	  * If an object of class2 wants to use properties(variables and methods) of class1 then
-	  * class2 has to use keyword extend
-	  */
 	
 }
 }
