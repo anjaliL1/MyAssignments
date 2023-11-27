458523d6d2318fe4f9718342c653c5320295cac0
anjaliL1 <151993042+anjaliL1@users.noreply.github.com>
2023-11-27 21:42:21 +0530

    Add files via upload

---
+++ week2/day1/Report.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		StudentClass s = new StudentClass();
+		System.out.println(s.studentName);
+		System.out.println(s.rollNo);
+		System.out.println(s.collegeName);
+		System.out.println(s.markScored);
+		System.out.println(s.cgpa);
+	
+	}
+}
---
+++ week2/day1/StudentClass.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class StudentClass {
+	
+	String studentName="Anjali";
+	int rollNo = 3;
+	String collegeName="ABCD";
+	float markScored=83.5f;
+	float cgpa=8.3f;
+	
+}
+
